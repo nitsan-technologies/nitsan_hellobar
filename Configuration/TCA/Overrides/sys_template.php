3dd34d4151e2bd172e28286b1fdf5577915a3384
Nilesh Malankiya <mnilesh.nitsan@gmail.com>
2022-05-30 16:02:38 +0530

    :tada: code cleanup and improvements

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,2 @@
+<?php
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('nitsan_hellobar', 'Configuration/TypoScript', 'NITSAN - API HelloBar.com');
--- ext_localconf.php
+++ ext_localconf.php
@@ -2,3 +2,4 @@
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
+$_EXTKEY = 'nitsan_hellobar';
