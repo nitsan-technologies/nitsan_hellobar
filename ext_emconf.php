ab2466caa2059d1604389e38db9440a6617002a1
NITSAN Technologies <sanjay@nitsan.in>
2022-08-08 18:40:10 +0530

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => '[NITSAN] HelloBar.com Plugin',
-    'description' => 'An API Integration for HelloBar.com - One of the most popular site! Integrate hellobar panel(s) with simple configuration from TYPO3 Backend & Displaying it at global to your TYPO3 site! Live-Demo: https://demo.t3terminal.com/t3t-extensions/ You can download PRO version for more-features & free-support at https://t3terminal.com/tyop3-hellobar-api-plugin-free/',
+    'description' => 'An API Integration for HelloBar.com - One of the most popular site! Integrate hellobar panel(s) with simple configuration from TYPO3 Backend & Displaying it at global to your TYPO3 site! Live-Demo: https://demo.t3terminal.com/t3t-extensions/ You can download PRO version for more-features & free-support at https://t3planet.com/tyop3-hellobar-api-plugin-free/',
     'category' => 'plugin',
     'author' => 'NITSAN Technologies Pvt Ltd',
     'author_email' => 'sanjay@nitsan.in',
