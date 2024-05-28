a676b857d5b6e2736a031e00a7a205938cad87f3
Harsora Krupa <harsorakrupa.nitsan@gmail.com>
2024-05-28 15:45:28 +0530

    [TODO] Remove nitsan and code clenup

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -2,4 +2,8 @@
 
 defined('TYPO3_MODE') || defined('TYPO3') or die();
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('nitsan_hellobar', 'Configuration/TypoScript', '[NITSAN] - API HelloBar.com');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'nitsan_hellobar',
+    'Configuration/TypoScript',
+    'API HelloBar.com'
+);
