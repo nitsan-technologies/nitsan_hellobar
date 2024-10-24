59d2231821e478e73111abbaa2639d7b7bba604d
Khushi Rupera <khushirupera.nitsan@gmail.com>
2024-10-24 15:30:34 +0530

    [TASK]Change extension version

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,10 +14,10 @@ $EM_CONF['nitsan_hellobar'] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '6.1.0',
+    'version' => '13.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.0.0-13.9.99',
+            'typo3' => '6.0.0-13.9.99',
         ],
         'conflicts' => [
         ],
