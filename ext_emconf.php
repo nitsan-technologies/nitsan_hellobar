d9db1925bfc1fd13e5d4a36133b3922d25bba3ea
NITSAN <info@nitsan.in>
2025-06-25 12:31:47 +0530

    Update ext_emconf.php
    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,13 @@
 <?php
 
 $EM_CONF['nitsan_hellobar'] = [
-    'title' => 'HelloBar',
-    'description' => 'The HelloBar TYPO3 Extension is a valuable tool for configuring and designing messages on your website. It provides tools and services to schedule messages for your guests exactly when you want them to see the messages. With Hello Bar, you can attract guests to your website and improve user engagement.
+    'title' => 'TYPO3 HelloBar – Smart Notification Bar',
+    'description' => 'The TYPO3 HelloBar Extension helps you schedule and display engaging notification messages to your visitors, boosting visibility and user interaction.',
     
-    *** Live-Demo: https://demo.t3planet.com/t3-extensions/hellobar *** Documentation & Free Support: https://t3planet.com/typo3-hellobar-extension',
     'category' => 'plugin',
-    'author' => 'T3: Nilesh Malankiya, T3: Himanshu Ramavat, QA: Krishna Dhapa',
-    'author_email' => 'sanjay@nitsan.in',
-    'author_company' => 'T3Planet // NITSAN',
+    'author' => 'Team T3Planet',
+    'author_email' => 'info@t3planet.de',
+    'author_company' => 'T3Planet',
     'state' => 'stable',
     'internal' => '',
     'uploadfolder' => '0',
